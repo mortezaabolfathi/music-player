65366217bbb6e6d57e2a39cf7fc22150a47f5ea2
Morteza <morteza.abolfathi73@gmail.com>
2022-11-22 18:16:07 +0330

    add redux toolkit to the project

---
+++ src/appRedux/features/music/MusicSlice.jsx
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+export const MusicSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+  
+  },
+})
+
+export const {  } = MusicSlice.actions
+
+export default MusicSlice.reducer
